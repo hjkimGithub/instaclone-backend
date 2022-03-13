a76a812856ba1e112f5e410919710e911ffa293a
hjkim <rlaguswo3408@gmail.com>
2022-03-13 20:46:02 +0900

    simple dotenv POC

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// import dotenv from "dotenv";
+require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 // import { typeDefs, resolvers } from "./schema";
 import schema from "./schema";
@@ -5,6 +7,9 @@ import schema from "./schema";
 const server = new ApolloServer({
     schema,
 });
+
+const PORT = process.env.PORT
+
 server
-  .listen()
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen(PORT)
+  .then(() => console.log(`Server is running on http://localhost:${PORT}/`));
