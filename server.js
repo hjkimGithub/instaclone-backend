d7a2f51f354343ce4eab018d28a3e9eefe17bdb8
hjkim <rlaguswo3408@gmail.com>
2022-03-13 20:14:16 +0900

    simple divide and conquer practice

--- server.js
+++ server.js
@@ -1,68 +1,11 @@
-// const {ApolloServer, gql} = require("apollo-server");
-import {ApolloServer, gql} from "apollo-server";
-import {PrismaClient} from "@prisma/client";
+import { ApolloServer, gql } from "apollo-server";
 
-const client = new PrismaClient();
-
-const typeDefs = gql `
-    type Movie {
-        id: Int!
-        title: String!
-        year: Int!
-        genre: String
-        createdAt: String!
-        updatedAt: String!
-    }
-    type Query {
-        movies: [Movie]
-        movie(id: Int!): Movie
-    }
-    type Mutation {
-        createMovie(title:String!, year:Int!, genre:String): Movie
-        deleteMovie(id: Int!): Movie
-        updateMovie(id: Int!, year: Int!): Movie
-    }
-`;
-
-const resolvers = {
-    Query: {
-        movies: () => client.movie.findMany(),
-        movie: (_, {id}) => client.movie.findUnique({
-            where: {
-                id
-            }
-        })
-    },
-    Mutation: {
-        // createMovie: (root, args, context, info) => "",
-        createMovie: (_, {title, year, genre}) => client.movie.create({
-            data: {
-                title,
-                year,
-                genre,
-            },
-        }),
-        deleteMovie: (_, { id }) => client.movie.delete({
-             where: { 
-                 id 
-            } 
-        }),
-        updateMovie: (_, { id, year }) => client.movie.update({ 
-            where: { 
-                id 
-            }, 
-            data: { 
-                year 
-            } 
-        }),
-    }
-};
+import { typeDefs, resolvers } from "./schema";
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
-
 server
-.listen()
-.then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));
