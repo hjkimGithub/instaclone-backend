178bd49aea405d8f5802e08a9d419d15beacf5e4
hjkim <rlaguswo3408@gmail.com>
2022-03-14 19:06:17 +0900

    simple seeing profile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -7,34 +7,41 @@ export default {
             _, 
             { firstName, lastName, username, email, password } 
             ) => {
-                // check if username or email are already on DB
-                const existingUser = await client.user.findFirst({
-                    where: {
-                        OR: [
-                            {
-                                username,
-                            },
-                            {
-                                email,
-                            },
-                        ],
-                    },
-                });
-                // hash password
-                const uglyPassword = await bcrypt.hash(password, 10);
-                // save, return the user
-                return client.user.create({
-                    data: {
-                        username,
-                        email,
-                        firstName,
-                        lastName,
-                        password: uglyPassword,
+               try {
+                    // check if username or email are already on DB
+                    const existingUser = await client.user.findFirst({
+                        where: {
+                            OR: [
+                                {
+                                    username,
+                                },
+                                {
+                                    email,
+                                },
+                            ],
+                        },
+                    });
+                    if(existingUser) {
+                        throw new Error("This username/ email is already taken.");
                     }
-                })
-                // same as
-                // const user = await ~~~
-                // return user
+                    // hash password
+                    const uglyPassword = await bcrypt.hash(password, 10);
+                    // save, return the user
+                    return client.user.create({
+                        data: {
+                            username,
+                            email,
+                            firstName,
+                            lastName,
+                            password: uglyPassword,
+                        }
+                    })
+                    // same as
+                    // const user = await ~~~
+                    // return user
+               } catch(e) {
+                    return e;
+               }
             },
     },
 };
