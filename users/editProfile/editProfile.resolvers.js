2cf9edf409cbcf2f3ad1349d03fb95ad8423b3e3
hjkim <rlaguswo3408@gmail.com>
2022-03-15 11:35:00 +0900

    editProfile await is needed for every prisma client!

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,44 @@
+import * as bcrypt from 'bcrypt';
+import client from "../../client";
+
 export default {
     Mutation: {
-        editProfile: () => console.log("hi"),
+        editProfile: async (
+            _, 
+            {
+                firstName,
+                lastName,
+                username,
+                email,
+                password: newPassword
+            }
+        ) => {
+            let uglyPassword = null;
+            if(newPassword) {
+                uglyPassword = await bcrypt.hash(newPassword, 10);
+            }
+            const updatedUser = await client.user.update({
+                where: {
+                    id: 2
+                },
+                data: {
+                    firstName,
+                    lastName,
+                    username,
+                    email,
+                    ...(uglyPassword && {password : uglyPassword}),
+                },
+            });
+            if(updatedUser.id) {
+                return {
+                    ok: true,
+                };
+            } else {
+                return {
+                    ok: false,
+                    error: "Could not update Profile",
+                };
+            }
+        },
     },
 }
