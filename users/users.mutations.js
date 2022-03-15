92f628d5f8d165b63d58e1d4becd4af817745b80
hjkim <rlaguswo3408@gmail.com>
2022-03-15 10:03:04 +0900

    backend login handle wrong username, wrong password jwt: issue login token

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import client from "../client";
+import jwt from "jsonwebtoken";
 
 export default {
     Mutation: {
@@ -43,5 +44,33 @@ export default {
                     return e;
                }
             },
+        login: async(_, {username, password}) => {
+            // find user with args.username
+            const user = await client.user.findFirst({
+                where: {
+                    username,
+                },
+            })
+            if(!user) {
+                return {
+                    ok: false,
+                    error: "User Not Found!",
+                };
+            }
+            // check password with  args.password
+            const passwordOk = await bcrypt.compare(password, user.password);
+            if(!passwordOk){
+                return {
+                    ok: false,
+                    error: "Incorrect Password!"
+                }
+            }
+            // issue token, send it to the user
+            const token = await jwt.sign({id:user.id}, process.env.SECRET_KEY);
+            return{
+                ok: true,
+                token,
+            };
+        },
     },
 };
