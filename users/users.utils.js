f5860d0ffc829594191d3dd1f7941548603ea086
hjkim <rlaguswo3408@gmail.com>
2022-03-25 15:35:40 +0900

    protectedResolver REfractor root, args, context, info recap

--- users/users.utils.js
+++ users/users.utils.js
@@ -22,11 +22,17 @@ export const getUser = async(token) => {
 
 export function protectedResolver(ourResolver){
     return function(root, args, context, info) {
+        // console.log(info);
         if(!context.loggedInUser){
-            return {
-                ok: false,
-                error: "Please log in to perform this action.",
-            };
+            const query = info.operation.operation === "query"
+            if(query){
+                return null 
+            } else {
+                return {
+                    ok: false,
+                    error: "Please log in to perform this action.",
+                };    
+            }
         }
         return ourResolver(root, args, context, info);
     };
