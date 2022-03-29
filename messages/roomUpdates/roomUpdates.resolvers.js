c2af15e57da26b5090636af3627280d70345ae44
hjkim <rlaguswo3408@gmail.com>
2022-03-29 20:36:21 +0900

    filter1: filter only for listening roomId

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,10 +1,17 @@
+import { withFilter } from "apollo-server-express";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
     Subscription: {
         roomUpdates: {
-            subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+            subscribe:  withFilter(
+                () => pubsub.asyncIterator(NEW_MESSAGE),
+                //payloads, variables
+                ({roomUpdates}, {id}) => {
+                    return roomUpdates.roomId === id;
+                }
+            ),
         }
     }
 }
